ba29653022695104ee2a62aef8e2ee6b66707517
athlony2k <100151282+athlony2k@users.noreply.github.com>
2022-02-21 15:53:40 -0500

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "image-store-1" {
   name     = "cf-random-test-image-store-bucket-1"
   location = "europe-north1"
 }
+
+resource "google_storage_bucket" "image-store-1" {
+  name     = "cf-random-test-image-store-bucket-2"
+  location = "asia-east1"
+}
