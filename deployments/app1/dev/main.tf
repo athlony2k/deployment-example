75c5e36442087585b22892f22eb43838a6057c2d
athlony2k <100151282+athlony2k@users.noreply.github.com>
2022-02-21 17:44:54 -0500

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -2,6 +2,8 @@ resource "google_storage_bucket" "image-store-2" {
   name     = "cf-random-test-image-store-bucket-2"
   location = "asia-east1"
   
+  encryption {
   default_kms_key_name = "dataowner_tw"
-}
 
+  }
+}
