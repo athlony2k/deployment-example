78feb6d7d6e896b60f44ba1f1e3307bd77eddedb
athlony2k <100151282+athlony2k@users.noreply.github.com>
2022-02-21 15:55:16 -0500

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "image-store-1" {
   location = "europe-north1"
 }
 
-resource "google_storage_bucket" "image-store-1" {
+resource "google_storage_bucket" "image-store-2" {
   name     = "cf-random-test-image-store-bucket-2"
   location = "asia-east1"
 }
