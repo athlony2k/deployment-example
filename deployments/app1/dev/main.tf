783a81aa00f30dbc5f63aed6ada911e962e31819
athlony2k <100151282+athlony2k@users.noreply.github.com>
2022-08-08 18:36:07 -0400

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,9 +1,14 @@
-resource "google_storage_bucket" "image-store-2" {
-  name     = "cf-random-test-image-store-bucket-2"
-  location = "asia-east1"
-  
-  encryption {
-  default_kms_key_name = "dataowner_tw"
+#resource "google_storage_bucket" "image-store-2" {
+#  name     = "cf-random-test-image-store-bucket-2"
+#  location = "asia-east1"
+#  encryption {
+#  default_kms_key_name = "dataowner_tw"
+#  }
+#}
 
-  }
+resource "google_project_iam_custom_role" "custom_roles" {
+  role_id     = "demo_app_cust_role"
+  title       = "Demo Custom Role"
+  description = "customer roles creation for demo"
+  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
 }
